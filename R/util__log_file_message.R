ae1f835cbe2c9c2a093be37a9e56faf4af0032cd
stemicha <stephan.michalik@gmx.de>
2024-12-20 08:03:32 +0100

    add system time stamp to log file entries

--- R/util__log_file_message.R
+++ R/util__log_file_message.R
@@ -12,34 +12,34 @@ message_function <- function(text = "", color = "blue", log_file_name = ""){
 
   if(color == "blue"){
     message(crayon::blue(text))
-    write(x = paste0("MESSAGE: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " MESSAGE: ",text),file = log_file_name,append = T)
   }
   if(color == "red"){
     message(crayon::red(text))
-    write(x = paste0("ERROR: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " ERROR: ",text),file = log_file_name,append = T)
   }
   if(color == "green"){
     message(crayon::green(text))
-    write(x = paste0("MESSAGE: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " MESSAGE: ",text),file = log_file_name,append = T)
   }
   if(color == "yellow"){
     message(crayon::yellow(text))
-    write(x = paste0("WARNING: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " WARNING: ",text),file = log_file_name,append = T)
   }
   if(color == "magenta"){
     message(crayon::magenta(text))
-    write(x = paste0("MESSAGE: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " MESSAGE: ",text),file = log_file_name,append = T)
   }
   if(color == "cyan"){
     message(crayon::cyan(text))
-    write(x = paste0("MESSAGE: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " MESSAGE: ",text),file = log_file_name,append = T)
   }
   if(color == "white"){
     message(crayon::white(text))
-    write(x = paste0("MESSAGE: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " MESSAGE: ",text),file = log_file_name,append = T)
   }
   if(color == "silver"){
     message(crayon::silver(text))
-    write(x = paste0("MESSAGE: ",text),file = log_file_name,append = T)
+    write(x = paste0(format(Sys.time(), "%d.%m.%Y %X"), " MESSAGE: ",text),file = log_file_name,append = T)
   }
 }
