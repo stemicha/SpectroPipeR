66057bfa2af1f99ee9cbb7cd4171ee2cacefce09
stemicha <stephan.michalik@gmx.de>
2026-03-25 13:18:49 +0100

    add condition length option:  width adjustment of plots width +  ((number_of_conditions %/% 15) * 2)

--- R/util__ggsave_pdf_png.R
+++ R/util__ggsave_pdf_png.R
@@ -3,6 +3,7 @@
 #' @param filename filename
 #' @param plot plot
 #' @param limitsize limitsize
+#' @param number_of_conditions number of condition add 2 inch /// width +  ((number_of_conditions %/% 15) * 2)
 #' @param width width
 #' @param height height
 #' @param dpi dpi
@@ -14,22 +15,44 @@
 ggsave_pdf_png <- function(filename = filename,
                            plot = plot,
                            limitsize = F,
+                           number_of_conditions = NULL,
                            width = width,
                            height = height,
                            dpi = 300){
-  #png
-  ggsave(filename = paste(filename,".png",sep = ""),
-         plot = plot,
-         limitsize = limitsize,
-         width = width,
-         height = height,
-         dpi = dpi,
-         device="png")
-  #pdf
-  ggsave(filename = paste(filename,".pdf",sep = ""),
-         plot = plot,
-         limitsize = limitsize,
-         width = width,
-         height = height,
-         device="pdf")
+
+  if(is.null(number_of_conditions)){
+    #png
+    ggsave(filename = paste(filename,".png",sep = ""),
+           plot = plot,
+           limitsize = limitsize,
+           width = width,
+           height = height,
+           dpi = dpi,
+           device="png")
+    #pdf
+    ggsave(filename = paste(filename,".pdf",sep = ""),
+           plot = plot,
+           limitsize = limitsize,
+           width = width,
+           height = height,
+           device="pdf")
+  }else{
+    #png
+    ggsave(filename = paste(filename,".png",sep = ""),
+           plot = plot,
+           limitsize = limitsize,
+           width = width +  ((number_of_conditions %/% 15) * 2),
+           height = height,
+           dpi = dpi,
+           device="png")
+    #pdf
+    ggsave(filename = paste(filename,".pdf",sep = ""),
+           plot = plot,
+           limitsize = limitsize,
+           width = width + ((number_of_conditions %/% 15) * 2),
+           height = height,
+           device="pdf")
+
+  }
+
 }
